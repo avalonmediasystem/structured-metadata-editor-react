1913e5023c5ae7610aefe5027dddffac5dd0d37c
Adam Arling <adam.arling@northwestern.edu>
2018-10-25 11:34:28 -0500

    Remove hanging redux-form reference in reducer file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
 import showForms from './show-forms';
 import smData from './sm-data';
 
 export default combineReducers({
   showForms,
-  smData,
-  form: formReducer
+  smData
 });
