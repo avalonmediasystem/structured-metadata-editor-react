94f00127201129c54b0388c91824732159bde929
Adam Arling <adam.arling@northwestern.edu>
2018-12-10 08:32:08 -0600

    Updates

---
+++ precache-manifest.c49e4395a42f12cf4838d25ff56a5526.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ac5a4415128a7044bdb",
+    "url": "/structured-metadata-editor-react/static/js/runtime~main.8ac5a441.js"
+  },
+  {
+    "revision": "195a8a334c3dc8208b96",
+    "url": "/structured-metadata-editor-react/static/js/main.195a8a33.chunk.js"
+  },
+  {
+    "revision": "05b8deb5c29346d14a58",
+    "url": "/structured-metadata-editor-react/static/js/1.05b8deb5.chunk.js"
+  },
+  {
+    "revision": "195a8a334c3dc8208b96",
+    "url": "/structured-metadata-editor-react/static/css/main.f2a1b16d.chunk.css"
+  },
+  {
+    "revision": "faab96000f1783c5ab1b9b29e3af4bd1",
+    "url": "/structured-metadata-editor-react/index.html"
+  }
+];
