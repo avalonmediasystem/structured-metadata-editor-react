75c307ff363bda3c96a6ea5af5d51646e153f3dc
Adam Arling <adam.arling@northwestern.edu>
2019-01-24 11:14:04 -0600

    Add ids to initial data structure on network request

--- src/containers/GenerateStructureContainer.js
+++ src/containers/GenerateStructureContainer.js
@@ -3,32 +3,56 @@ import { connect } from 'react-redux';
 import * as actions from '../actions/sm-data';
 import APIUtils from '../api/Utils';
 import * as showFormActions from '../actions/show-forms';
+import uuidv1 from 'uuid/v1';
 
 const apiUtils = new APIUtils();
 
 class GenerateStructureContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.smData = [];
+  async componentDidMount() {
+    try {
+      const response = await apiUtils.getRequest('structure.json');
+      let structureJS = response.data;
+
+      // Add unique ids to every object
+      let smData = this.addIds([structureJS]);
+
+      // Update the redux store
+      this.props.buildSMUI(smData);
+    } catch (error) {
+      if (error.response !== undefined) {
+        this.props.handleResponse(error.response.status);
+      } else if (error.request !== undefined) {
+        this.props.handleResponse(error.request.status);
+      } else {
+        this.props.handleResponse(-1);
+      }
+    }
   }
 
-  async componentDidMount() {
-    await apiUtils
-      .getRequest('structure.json')
-      .then(response => {
-        const structureJS = response.data;
-        this.smData = [structureJS];
-        this.props.buildSMUI(this.smData);
-      })
-      .catch(error => {
-        if (error.response !== undefined) {
-          this.props.handleResponse(error.response.status);
-        } else if (error.request !== undefined) {
-          this.props.handleResponse(error.request.status);
-        } else {
-          this.props.handleResponse(-1);
+  /**
+   * This function adds a unique, front-end only id, to every object in the data structure
+   * @param {Array} structureJS
+   * @returns {Array}
+   */
+  addIds(structureJS) {
+    let structureWithIds = [...structureJS];
+
+    // Recursively loop through data structure
+    let fn = items => {
+      for (let item of items) {
+        // Create and add an id
+        item.id = uuidv1();
+
+        // Send child items back into the function
+        if (item.items && item.items.length > 0) {
+          fn(item.items);
         }
-      });
+      }
+    };
+
+    fn(structureWithIds);
+
+    return structureWithIds;
   }
 
   render() {
