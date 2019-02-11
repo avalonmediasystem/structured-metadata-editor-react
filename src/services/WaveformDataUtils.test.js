46112bb1589d909a75292e3a9304979e0e1580cb
Dananji <dan9131@gmail.com>
2019-02-11 16:32:35 -0500

    Changed peaks.js mock in Jest for peaks initialization

--- src/services/WaveformDataUtils.test.js
+++ src/services/WaveformDataUtils.test.js
@@ -7,31 +7,14 @@ import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { testMetadataStructure } from '../test/TestStructure';
 
+jest.mock('peaks.js');
+import mockPeaks from 'peaks.js';
+
 const waveformUtils = new WaveformDataUtils();
 const mockStore = configureMockStore([thunk]);
 
-jest.mock('peaks.js', () => {
-  const Peaks = jest.fn(opts => {
-    let peaks = {};
-    peaks.options = {
-      ...opts,
-      dataUri: 'http://localhost:3123/data/mock-response-waveform.json',
-      dataUriDefaultFormat: 'json',
-      pointMarkerColor: '#006eb0',
-      showPlayheadTime: true,
-      zoomWaveformColor: 'rgba(217, 217, 217, 1)'
-    };
-    return peaks;
-  });
-  return {
-    init: jest.fn(opts => {
-      return Peaks(opts);
-    })
-  };
-});
-
 describe('WaveformDataUtils class', () => {
-  let store, options;
+  let store, options, waveformRef, audioRef;
   beforeEach(() => {
     store = mockStore({});
     const waveformContainer = React.createRef();
@@ -46,8 +29,8 @@ describe('WaveformDataUtils class', () => {
         />
       </Provider>
     );
-    const waveformRef = wrapper.find('#waveform-container').instance();
-    const audioRef = wrapper.find('audio').instance();
+    waveformRef = wrapper.find('#waveform-container').instance();
+    audioRef = wrapper.find('audio').instance();
     options = {
       container: waveformRef,
       mediaElement: audioRef
@@ -55,45 +38,75 @@ describe('WaveformDataUtils class', () => {
   });
 
   test('initializes peaks with empty metadata structure', () => {
+    mockPeaks.init.mockImplementationOnce(() => {
+      return {
+        options: {
+          container: waveformRef,
+          mediaElement: audioRef,
+          dataUri: 'http://localhost:3123/data/mock-response-waveform.json',
+          dataUriDefaultFormat: 'json',
+          segments: []
+        }
+      };
+    });
     const value = waveformUtils.initPeaks([], options);
     expect(value).toBeDefined();
     expect(value.options.dataUri).toBe(
       'http://localhost:3123/data/mock-response-waveform.json'
     );
     expect(value.options.segments).toEqual([]);
+    expect(mockPeaks.init).toHaveBeenCalledTimes(1);
   });
   test('initializes peaks with metadata structure', () => {
+    mockPeaks.init.mockImplementationOnce(() => {
+      return {
+        options: {
+          container: waveformRef,
+          mediaElement: audioRef,
+          dataUri: 'http://localhost:3123/data/mock-response-waveform.json',
+          dataUriDefaultFormat: 'json',
+          segments: [
+            {
+              startTime: 3.32,
+              endTime: 10.32,
+              editable: false,
+              labelText: 'Segment 1.1',
+              id: '123a-456b-789c-3d',
+              color: '#80A590'
+            },
+            {
+              startTime: 11.23,
+              endTime: 480,
+              editable: false,
+              labelText: 'Segment 1.2',
+              id: '123a-456b-789c-4d',
+              color: '#2A5459'
+            },
+            {
+              startTime: 543.24,
+              endTime: 900,
+              editable: false,
+              labelText: 'Segment 2.1',
+              id: '123a-456b-789c-8d',
+              color: '#80A590'
+            }
+          ]
+        }
+      };
+    });
     const value = waveformUtils.initPeaks(testMetadataStructure, options);
-    const expected = [
-      {
-        startTime: 3.32,
-        endTime: 10.32,
-        editable: false,
-        labelText: 'Segment 1.1',
-        id: '123a-456b-789c-3d',
-        color: '#80A590'
-      },
-      {
-        startTime: 11.23,
-        endTime: 480,
-        editable: false,
-        labelText: 'Segment 1.2',
-        id: '123a-456b-789c-4d',
-        color: '#2A5459'
-      },
-      {
-        startTime: 543.24,
-        endTime: 900,
-        editable: false,
-        labelText: 'Segment 2.1',
-        id: '123a-456b-789c-8d',
-        color: '#80A590'
-      }
-    ];
     expect(value).toBeDefined();
     expect(value.options.dataUri).toBe(
       'http://localhost:3123/data/mock-response-waveform.json'
     );
-    expect(value.options.segments).toEqual(expected);
+    expect(value.options.segments).toHaveLength(3);
+    expect(value.options.segments).toContainEqual({
+      startTime: 11.23,
+      endTime: 480,
+      editable: false,
+      labelText: 'Segment 1.2',
+      id: '123a-456b-789c-4d',
+      color: '#2A5459'
+    });
   });
 });
