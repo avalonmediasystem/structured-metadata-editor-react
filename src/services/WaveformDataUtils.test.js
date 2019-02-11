3b71646e4f2a8d3a3aa151048cf61eafad5693b2
Dananji <dan9131@gmail.com>
2019-02-11 16:58:50 -0500

    Refined init peaks test

--- src/services/WaveformDataUtils.test.js
+++ src/services/WaveformDataUtils.test.js
@@ -14,9 +14,9 @@ const waveformUtils = new WaveformDataUtils();
 const mockStore = configureMockStore([thunk]);
 
 describe('WaveformDataUtils class', () => {
-  let store, options, waveformRef, audioRef;
+  let waveformRef, audioRef;
   beforeEach(() => {
-    store = mockStore({});
+    const store = mockStore({});
     const waveformContainer = React.createRef();
     const mediaPlayer = React.createRef();
     const wrapper = mount(
@@ -31,26 +31,23 @@ describe('WaveformDataUtils class', () => {
     );
     waveformRef = wrapper.find('#waveform-container').instance();
     audioRef = wrapper.find('audio').instance();
-    options = {
-      container: waveformRef,
-      mediaElement: audioRef
-    };
   });
 
   test('initializes peaks with empty metadata structure', () => {
-    mockPeaks.init.mockImplementationOnce(() => {
+    mockPeaks.init.mockImplementationOnce(opts => {
       return {
         options: {
+          ...opts,
           container: waveformRef,
           mediaElement: audioRef,
           dataUri: 'http://localhost:3123/data/mock-response-waveform.json',
-          dataUriDefaultFormat: 'json',
-          segments: []
+          dataUriDefaultFormat: 'json'
         }
       };
     });
-    const value = waveformUtils.initPeaks([], options);
+    const value = waveformUtils.initPeaks([], {});
     expect(value).toBeDefined();
+    expect(value.options.container).not.toBeNull();
     expect(value.options.dataUri).toBe(
       'http://localhost:3123/data/mock-response-waveform.json'
     );
@@ -58,47 +55,49 @@ describe('WaveformDataUtils class', () => {
     expect(mockPeaks.init).toHaveBeenCalledTimes(1);
   });
   test('initializes peaks with metadata structure', () => {
-    mockPeaks.init.mockImplementationOnce(() => {
+    mockPeaks.init.mockImplementationOnce(opts => {
       return {
         options: {
+          ...opts,
           container: waveformRef,
           mediaElement: audioRef,
           dataUri: 'http://localhost:3123/data/mock-response-waveform.json',
-          dataUriDefaultFormat: 'json',
-          segments: [
-            {
-              startTime: 3.32,
-              endTime: 10.32,
-              editable: false,
-              labelText: 'Segment 1.1',
-              id: '123a-456b-789c-3d',
-              color: '#80A590'
-            },
-            {
-              startTime: 11.23,
-              endTime: 480,
-              editable: false,
-              labelText: 'Segment 1.2',
-              id: '123a-456b-789c-4d',
-              color: '#2A5459'
-            },
-            {
-              startTime: 543.24,
-              endTime: 900,
-              editable: false,
-              labelText: 'Segment 2.1',
-              id: '123a-456b-789c-8d',
-              color: '#80A590'
-            }
-          ]
+          dataUriDefaultFormat: 'json'
         }
       };
     });
-    const value = waveformUtils.initPeaks(testMetadataStructure, options);
+    const expected = [
+      {
+        startTime: 3.32,
+        endTime: 10.32,
+        editable: false,
+        labelText: 'Segment 1.1',
+        id: '123a-456b-789c-3d',
+        color: '#80A590'
+      },
+      {
+        startTime: 11.23,
+        endTime: 480,
+        editable: false,
+        labelText: 'Segment 1.2',
+        id: '123a-456b-789c-4d',
+        color: '#2A5459'
+      },
+      {
+        startTime: 543.24,
+        endTime: 900,
+        editable: false,
+        labelText: 'Segment 2.1',
+        id: '123a-456b-789c-8d',
+        color: '#80A590'
+      }
+    ];
+    const value = waveformUtils.initPeaks(testMetadataStructure, {});
     expect(value).toBeDefined();
     expect(value.options.dataUri).toBe(
       'http://localhost:3123/data/mock-response-waveform.json'
     );
+    expect(value.options.segments).toEqual(expected);
     expect(value.options.segments).toHaveLength(3);
     expect(value.options.segments).toContainEqual({
       startTime: 11.23,
