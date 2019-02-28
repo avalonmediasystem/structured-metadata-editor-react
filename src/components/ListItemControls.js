930487e956166c163777a5b09dbc46780eef8eb5
Adam Arling <adam.arling@northwestern.edu>
2019-02-28 12:13:50 +0100

    Update to put click handler on the buttons for a list item controls, instead of the icon elements.

--- src/components/ListItemControls.js
+++ src/components/ListItemControls.js
@@ -14,26 +14,36 @@ const ListItemControls = props => {
     handleDelete,
     showForms
   } = props;
+
   return (
     <div className="edit-controls-wrapper">
       {itemType === 'span' && (
         <OverlayTrigger placement="left" overlay={tooltip('Show drop targets')}>
-          <Button bsStyle="link">
-            <FontAwesomeIcon
-              icon="dot-circle"
-              onClick={handleShowDropTargetsClick}
-            />
+          <Button
+            bsStyle="link"
+            disabled={showForms.disabled}
+            onClick={handleShowDropTargetsClick}
+          >
+            <FontAwesomeIcon icon="dot-circle" />
           </Button>
         </OverlayTrigger>
       )}
       <OverlayTrigger placement="top" overlay={tooltip('Edit')}>
-        <Button bsStyle="link" disabled={showForms.disabled}>
-          <FontAwesomeIcon icon="pen" onClick={handleEditClick} />
+        <Button
+          bsStyle="link"
+          onClick={handleEditClick}
+          disabled={showForms.disabled}
+        >
+          <FontAwesomeIcon icon="pen" />
         </Button>
       </OverlayTrigger>
       <OverlayTrigger placement="right" overlay={tooltip('Delete')}>
-        <Button bsStyle="link" disabled={showForms.disabled}>
-          <FontAwesomeIcon icon="trash" onClick={handleDelete} />
+        <Button
+          bsStyle="link"
+          onClick={handleDelete}
+          disabled={showForms.disabled}
+        >
+          <FontAwesomeIcon icon="trash" />
         </Button>
       </OverlayTrigger>
     </div>
