1c9967efbb5d59ca4f58821759a9fadb31ac4f0e
Adam Arling <adam.arling@northwestern.edu>
2019-04-05 12:54:31 -0500

    Create a Root component file which the NPM packager use as the exported component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import rootReducer from './reducers';
 import registerServiceWorker from './registerServiceWorker';
-import thunk from 'redux-thunk';
+import Root from './Root';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
